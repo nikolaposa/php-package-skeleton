6533bface716c22c299c81bd3ceaec5506b98784
Nikola Posa <posa.nikola@gmail.com>
2017-01-29 10:52:19 +0100

    Added license header to PHP sources.

--- src/Example.php
+++ src/Example.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the :package_name package.
+ *
+ * Copyright (c) :vendor
+ *
+ * For full copyright and license information, please refer to the LICENSE file,
+ * located at the package root folder.
+ */
 
 namespace PackageSkeleton;
 
